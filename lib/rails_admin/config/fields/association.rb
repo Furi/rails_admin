60144d9ee58374842ed2ba715aea3e51ee8912e1
Joe Peric <peric.joe@gmail.com>
2014-09-26 16:01:57 -0400

    when showing account name in an index view, clicking on the account will bring them to the type of object their viewing, not the displayed associated account

--- lib/rails_admin/config/fields/association.rb
+++ lib/rails_admin/config/fields/association.rb
@@ -21,7 +21,13 @@ module RailsAdmin
             am = amc.abstract_model
             wording = associated.send(amc.object_label_method)
             can_see = !am.embedded? && (show_action = v.action(:show, am, associated))
-            can_see ? v.link_to(wording, v.url_for(action: show_action.action_name, model_name: am.to_param, id: associated.id), class: 'pjax') : wording
+            if am.model_name == "Account"
+              #use the current model instead of Account
+              result_url = v.url_for(action: show_action.action_name, model_name: bindings[:object].class.name, id: bindings[:object].id)
+            else
+              result_url = v.url_for(action: show_action.action_name, model_name: am.to_param, id: associated.id)
+            end
+            can_see ? v.link_to(wording, result_url, class: 'pjax') : wording
           end.to_sentence.html_safe
         end
 
